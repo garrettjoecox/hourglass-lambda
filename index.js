3f8a21a385812c8c0961ccc10a867d7c519349ac
Garrett Cox <garrettjcox@gmail.com>
2019-05-04 22:05:50 -0500

    remove debug run

--- index.js
+++ index.js
@@ -10,13 +10,3 @@ new CronJob('*/15 * * * *', async () => {
     console.error(error);
   }
 }, null, true, 'America/Los_Angeles');
-
-(async () => {
-  try {
-    const response = await handler();
-
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-})();
